f2e1c1188f62de46545824fdd4efafd266a74fb8
Vladimir <freshitpte@gmail.com>
2021-01-17 17:21:01 +0200

    add styles for stopwatch component

--- src/components/Stopwatch/index.jsx
+++ src/components/Stopwatch/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {startOfDay, format, addSeconds} from 'date-fns';
+import stopwatchstyle from './Stopwatch.module.css';
 
 function Stopwatch(){
   const [time, setTime] = useState(startOfDay(new Date()));
@@ -18,15 +19,15 @@ function Stopwatch(){
   }, [isRunning, time]);
 
   return(
-    <>
-    <h1>{format(time, 'HH:mm:ss')}</h1>
+    <article className={stopwatchstyle.wrapper}>
+    <h1 className={stopwatchstyle.time}>{format(time, 'HH:mm:ss')}</h1>
     <button onClick={() => {setIsRunning(!isRunning);}}>
       {isRunning ? 'stop': 'start'}
     </button>
     <button onClick={()=>{setTime(startOfDay(new Date()));}}>
       reset
     </button>
-    </>
+    </article>
   );
 }
 
